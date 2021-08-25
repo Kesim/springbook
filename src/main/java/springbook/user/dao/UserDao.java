0f36248c74df53b3eb53f4c5f4676d200856a186
Kang <tjdals9790@naver.com>
2021-08-25 12:45:07 +0900

    리팩토링 - makeConnection() 메소드를 인터페이스로 분리
    인터페이스를 두어 메소드 형식을 고정.
    UserDao 클래스에선 동일한 방법으로 makeConnection() 을 사용할 수 있음.
    
    인터페이스를 구현한 클래스를 UserDao 클래스 안에서 선택하므로,
    다른 클래스로 변경할 때마다 코드의 변경이 필요.

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -1,7 +1,6 @@
 package springbook.user.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,10 +8,14 @@ import java.sql.SQLException;
 import springbook.user.domain.User;
 
 public class UserDao {
-	private SimpleConnectionMaker simpleConnectionMaker;
+	private ConnectionMaker connectionMaker;
+	
+	public UserDao() {
+		connectionMaker = new SimpleConnectionMaker();
+	}
 	
 	public void add(User user) throws ClassNotFoundException, SQLException{
-		Connection c = simpleConnectionMaker.makeNewConnection();
+		Connection c = connectionMaker.makeConnection();
 		
 		PreparedStatement ps = c.prepareStatement(
 				"insert into users(id, name, password) values(?, ?, ?)");
@@ -27,7 +30,7 @@ public class UserDao {
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException{
-		Connection c = simpleConnectionMaker.makeNewConnection();
+		Connection c = connectionMaker.makeConnection();
 		
 		PreparedStatement ps = c.prepareStatement(
 				"select * from users where id = ?");
