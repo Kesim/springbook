b9e9c66731556a386a88142acbd3d896e65e5baa
Kang <tjdals9790@naver.com>
2021-08-31 18:43:01 +0900

    리팩토링 - UserDao.deleteAll() 에서 메서드로 분리한 콜백을 JdbcContext 클래스로 옮겨 분리
    분리해낸 콜백 메서드를 다른 Dao에서 공유하여 사용할 수 있도록 JdbcContext 클래스로 옮김.
    JdbcContext 클래스에 메서드들이 응집도 높게 모임.
    
    JdbcContext 클래스에는 클라이언트, 템플릿, 콜백이 모두 있게 됨.
    클라이언트 - workWithStatementStrategy() 호출
    콜백 - new StatementStrategy() 의 내용
    템플릿 - workWithStatementStrategy() 의 내용

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -67,7 +67,7 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		executeSql("delete from users");
+		this.jdbcContext.executeSql("delete from users");
 	}
 	
 	public int getCount() throws SQLException{
@@ -105,17 +105,5 @@ public class UserDao {
 				}
 			}
 		}
-	}
-	
-	private void executeSql(final String query) throws SQLException {
-		this.jdbcContext.workWithStatementStrategy(
-			new StatementStrategy() {
-				@Override
-				public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-					PreparedStatement ps = c.prepareStatement(query);
-					return ps;
-				}
-			}
-		);
 	}
 }
