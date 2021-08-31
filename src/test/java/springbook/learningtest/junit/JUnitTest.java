6dcc1542f0a9323020025cf7e5e693e3f6d2aa4e
Kang <tjdals9790@naver.com>
2021-08-31 13:58:22 +0900

    기능변경 - JUnitTest 에서 각 오브젝트를 비교하는 것을 더 확실하게 변경
    기존 방식은 바로 이전에 생성된 오브젝트와만 비교할 수 있었음.
    Set을 이용해 각 오브젝트를 저장한 후 비교하면 더 확실함.

--- src/test/java/springbook/learningtest/junit/JUnitTest.java
+++ src/test/java/springbook/learningtest/junit/JUnitTest.java
@@ -1,9 +1,11 @@
 package springbook.learningtest.junit;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -13,23 +15,23 @@ import org.junit.Test;
  * 각 테스트에서 새로 생성된 오브젝트를 static으로 저장한 기존 오브젝트와 비교하여 확인.
  */
 public class JUnitTest {
-	static JUnitTest testObject;
+	static Set<JUnitTest> testObjects = new HashSet<>();
 	
 	@Test
 	public void test1() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 	@Test
 	public void test2() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 	@Test
 	public void test3() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 }
