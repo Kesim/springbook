30aced45d830afd8afde5e335c7adec21127064e
Kang <tjdals9790@naver.com>
2021-09-03 16:45:17 +0900

    리팩토링 - UserDao 에서 반복사용된 RowMapper 콜백을 분리해 인스턴스 오브젝트로 보관
    userMapper을 빈으로 따로 분리하여 관리하면, User 의 필드명 변경이나 필드 개수 변경에 유연하게 대처할 수 있다.
    또한 sql문을 외부에서 읽어와서 사용한다면, 위와 같은 상황이나 sql 쿼리 최적화할 때 UserDao 코드를 수정하지 않아도 된다.

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -13,6 +13,18 @@ import springbook.user.domain.User;
 
 public class UserDao {
 	private JdbcTemplate jdbcTemplate;
+	
+	private RowMapper<User> userMapper =
+		new RowMapper<User>() {
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setid(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+				return user;
+			}
+		};
 
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -25,17 +37,7 @@ public class UserDao {
 
 	public User get(String id) throws SQLException{
 		return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-			new RowMapper<User>() {
-				@Override
-				public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-					User user = new User();
-					user.setid(rs.getString("id"));
-					user.setName(rs.getString("name"));
-					user.setPassword(rs.getString("password"));
-					return user;
-				}
-		}
-		, id);
+			this.userMapper, id);
 	}
 	
 	public void deleteAll() throws SQLException {
@@ -47,16 +49,6 @@ public class UserDao {
 	}
 	
 	public List<User> getAll() throws SQLException {
-		return this.jdbcTemplate.query("select * from users order by id",
-			new RowMapper<User>() {
-				@Override
-				public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-					User user = new User();
-					user.setid(rs.getString("id"));
-					user.setName(rs.getString("name"));
-					user.setPassword(rs.getString("password"));
-					return user;
-				}
-		});
+		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
 	}
 }
