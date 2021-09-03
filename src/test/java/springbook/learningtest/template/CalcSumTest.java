ac89714f9cb656ab9951e0358a96cf6f4b7a331c
Kang <tjdals9790@naver.com>
2021-09-03 10:53:05 +0900

    기능추가 - 파일의 데이터를 모두 곱하는 calcMultiply() 메서드 추가 및 관련 테스트 추가

--- src/test/java/springbook/learningtest/template/CalcSumTest.java
+++ src/test/java/springbook/learningtest/template/CalcSumTest.java
@@ -11,15 +11,20 @@ import org.junit.Test;
 public class CalcSumTest {
 	Calculator calculator;
 	String numFilepath;
-	
+
 	@Before
 	public void setUp() {
 		this.calculator = new Calculator();
 		this.numFilepath = getClass().getResource("/numbers.txt").getPath();
 	}
-	
+
 	@Test
-	public void sumOfNumbers() throws IOException{
+	public void sumOfNumbers() throws IOException {
 		assertThat(calculator.calcSum(numFilepath), is(10));
+	}
+
+	@Test
+	public void multiplyOfNumbers() throws IOException {
+		assertThat(calculator.calcMultiply(numFilepath), is(24));
 	}
 }
