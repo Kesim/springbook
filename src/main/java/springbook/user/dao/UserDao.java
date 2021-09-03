3cf5fdb6af61ce706a83f8ba10bf0965ba66bd2d
Kang <tjdals9790@naver.com>
2021-09-03 14:20:35 +0900

    기능변경 - UserDao 클래스에서 사용하던 JdbcContext 를 대신할 스프링의 JdbcTemplate 으로 교체
    교체하며 JdbcContext를 사용하던 메서드를 JdbcTemplate 을 사용하도록 수정.
    JdbcTemplate 오브젝트에 DataSource 오브젝트 DI를 xml 설정파일 대신 setDataSource() 에서 처리.

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -8,36 +8,22 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import springbook.user.domain.User;
 
 public class UserDao {
 	private DataSource dataSource;
-	private JdbcContext jdbcContext;
-	
+	private JdbcTemplate jdbcTemplate;
+
 	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 		this.dataSource = dataSource;
 	}
 	
-	public void setJdbcContext(JdbcContext jdbcContext) {
-		this.jdbcContext = jdbcContext;
-	}
-
 	public void add(final User user) throws SQLException{
-		this.jdbcContext.workWithStatementStrategy(
-			new StatementStrategy() {
-				@Override
-				public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-					PreparedStatement ps = c.prepareStatement(
-							"insert into users(id, name, password) values(?, ?, ?)");
-					ps.setString(1, user.getId());
-					ps.setString(2, user.getName());
-					ps.setString(3, user.getPassword());
-					
-					return ps;
-				}
-			}
-		);
+		this.jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)",
+			user.getId(), user.getName(), user.getPassword());
 	}
 
 	public User get(String id) throws SQLException{
@@ -67,7 +53,7 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		this.jdbcContext.executeSql("delete from users");
+		this.jdbcTemplate.update("delete from users");
 	}
 	
 	public int getCount() throws SQLException{
