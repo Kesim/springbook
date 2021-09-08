c95b4e44b30d3711cd4366ae84c17946069999dc
Kang <tjdals9790@naver.com>
2021-09-08 17:55:18 +0900

    기능추가 - 레벨 업그레이드 후 안내메일 전송 메서드 추가
    JavaMail API 를 이용하여 구현.

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -1,6 +1,15 @@
 package springbook.user.service;
 
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
@@ -35,13 +44,40 @@ public class UserService {
 			List<User> users = userDao.getAll();
 			for(User user : users) {
 				if(userLevelUpgradePolicy.canUpgradeLevel(user)) {
-					userLevelUpgradePolicy.upgradeLevel(user);
+					upgradeLevel(user);
 				}
 			}
 			this.transactionManager.commit(status);
 		} catch(RuntimeException e) {
 			this.transactionManager.rollback(status);
 			throw e;
+		}
+	}
+	
+	private void upgradeLevel(User user) {
+		userLevelUpgradePolicy.upgradeLevel(user);
+		sendUpgradeMail(user);
+	}
+	
+	private void sendUpgradeMail(User user) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "mail.ksug.org");
+		Session s = Session.getInstance(props, null);
+		
+		MimeMessage message = new MimeMessage(s);
+		try {
+			message.setFrom(new InternetAddress("useradmin@ksug.org"));
+			message.addRecipient(Message.RecipientType.TO,
+				new InternetAddress(user.getEmail()));
+			message.setSubject("Upgrade 안내");
+			message.setText("사용자님의 등급이 " + user.getLevel().name() +
+				"로 업그레이드되었습니다");
+			
+			Transport.send(message);
+		} catch(AddressException e) {
+			throw new RuntimeException();
+		} catch(MessagingException e) {
+			throw new RuntimeException();
 		}
 	}
 
