8d9f438a0438f19303bf0c8561bcc414157eb357
Kang <tjdals9790@naver.com>
2021-09-10 10:31:57 +0900

    리팩토링 - 다이내믹 프록시 확장
    다양한 타입의 오브젝트를 받아도 정상적으로 수행할 수 있도록 수정.
    범용적으로 사용할 수 있음.

--- src/main/java/springbook/learningtest/jdk/UppercaseHandler.java
+++ src/main/java/springbook/learningtest/jdk/UppercaseHandler.java
@@ -4,15 +4,18 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
-	Hello target;
+	Object target;
 	
-	public UppercaseHandler(Hello target) {
+	public UppercaseHandler(Object target) {
 		this.target = target;
 	}
 	
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		String result = (String)method.invoke(target, args);
-		return result.toUpperCase();
+		Object result = method.invoke(target, args);
+		if(result instanceof String) {
+			return ((String)result).toUpperCase();
+		}
+		return result;
 	}
 }
