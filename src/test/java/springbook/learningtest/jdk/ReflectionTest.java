253283f564c5afb0f1b5e7d82177d82a17bbf627
Kang <tjdals9790@naver.com>
2021-09-10 10:22:58 +0900

    기능추가 - 다이내믹 프록시 적용
    다이내믹 프록시의 메서드 하나로 여러 클래스, 여러 메서드에 적용할 수 있음.

--- src/test/java/springbook/learningtest/jdk/ReflectionTest.java
+++ src/test/java/springbook/learningtest/jdk/ReflectionTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 import org.junit.Test;
 
@@ -33,6 +34,17 @@ public class ReflectionTest {
 		assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
 		
 		Hello proxiedHello = new HelloUppercase(new HelloTarget());
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+	}
+	
+	@Test
+	public void dynamicProxy() {
+		Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+				getClass().getClassLoader(),
+				new Class[] { Hello.class },
+				new UppercaseHandler(new HelloTarget()));
 		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
 		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
 		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
