d9429b9551d51ed87cb3280861d1b7e13bad58a6
Kang <tjdals9790@naver.com>
2021-09-14 16:11:19 +0900

    기능변경 - 트랜잭션 어노테이션 사용
    트랜잭션 어노테이션 사용 설정 한줄이면 어노테이션 대상을 포인트컷에 추가시켜 트랜잭션을 적용해준다.

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -2,11 +2,16 @@ package springbook.user.service;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import springbook.user.domain.User;
 
+@Transactional
 public interface UserService {
 	void add(User user);
+	@Transactional(readOnly = true)
 	User get(String id);
+	@Transactional(readOnly = true)
 	List<User> getAll();
 	void update(User user);
 	void deleteAll();
