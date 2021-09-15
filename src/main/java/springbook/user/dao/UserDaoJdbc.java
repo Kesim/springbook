e8df97ac67a010c8f06e4957ad94fdf4cf7f8236
Kang <tjdals9790@naver.com>
2021-09-15 11:25:35 +0900

    리팩토링 - UserDaoJdbc 코드 정리
    인터페이스와 메소드 순서 일치시킴.

--- src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -15,56 +15,61 @@ import springbook.user.domain.User;
 public class UserDaoJdbc implements UserDao {
 	private JdbcTemplate jdbcTemplate;
 	
-	private RowMapper<User> userMapper =
-		new RowMapper<User>() {
-			@Override
-			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-				User user = new User();
-				user.setId(rs.getString("id"));
-				user.setName(rs.getString("name"));
-				user.setPassword(rs.getString("password"));
-				user.setEmail(rs.getString("email"));
-				user.setLevel(Level.valueOf(rs.getInt("level")));
-				user.setLogin(rs.getInt("login"));
-				user.setRecommend(rs.getInt("recommend"));
-				return user;
-			}
-		};
-
+	private RowMapper<User> userMapper = new RowMapper<User>() {
+		@Override
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+			user.setEmail(rs.getString("email"));
+			user.setLevel(Level.valueOf(rs.getInt("level")));
+			user.setLogin(rs.getInt("login"));
+			user.setRecommend(rs.getInt("recommend"));
+			return user;
+		}
+	};
+	
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	
-	public void add(final User user){
+	@Override
+	public void add(final User user) {
 		this.jdbcTemplate.update(
-			"insert into users(id, name, password, email, level, login, recommend) " +
-			"values(?, ?, ?, ?, ?, ?, ?)",
-			user.getId(), user.getName(), user.getPassword(), user.getEmail(),
-			user.getLevel().intValue(), user.getLogin(), user.getRecommend());
-	}
-
-	public User get(String id){
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-			this.userMapper, id);
+				"insert into users(id, name, password, email, level, login, recommend) "
+						+ "values(?, ?, ?, ?, ?, ?, ?)",
+				user.getId(), user.getName(), user.getPassword(), user.getEmail(),
+				user.getLevel().intValue(), user.getLogin(), user.getRecommend());
 	}
 	
-	public void deleteAll(){
+	@Override
+	public void deleteAll() {
 		this.jdbcTemplate.update("delete from users");
 	}
 	
-	public int getCount(){
+	@Override
+	public User get(String id) {
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", this.userMapper,
+				id);
+	}
+	
+	@Override
+	public List<User> getAll() {
+		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
+	}
+	
+	@Override
+	public int getCount() {
 		return this.jdbcTemplate.queryForInt("select count(*) from users");
 	}
 	
-	public List<User> getAll(){
-		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
-	}
-
 	@Override
 	public void update(User user) {
-		this.jdbcTemplate.update("update users set name = ?, password = ?, email = ?, " +
-			"level = ?, login = ?, recommend = ? where id = ?",
-			user.getName(), user.getPassword(), user.getEmail(), user.getLevel().intValue(),
-			user.getLogin(), user.getRecommend(), user.getId());
+		this.jdbcTemplate.update(
+				"update users set name = ?, password = ?, email = ?, "
+						+ "level = ?, login = ?, recommend = ? where id = ?",
+				user.getName(), user.getPassword(), user.getEmail(), user.getLevel().intValue(),
+				user.getLogin(), user.getRecommend(), user.getId());
 	}
 }
