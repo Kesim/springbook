e3d3e58f2530f7e424d75e41a66dd9390adb10ab
Kang <tjdals9790@naver.com>
2021-09-15 16:07:17 +0900

    리팩토링 - xml 파일명 분리, 초기화 후처리기 사용
    하드코딩 되어있던 xml 파일명을 DI 받도록 분리.
    예외처리가 있던 생성자를 대신할 메소드를 빈 후처리기가 호출하도록 설정.
    빈 오브젝트 생성 - 연관 오브젝트 주입 - 지정된 메소드 호출.

--- src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -14,13 +15,19 @@ import springbook.user.sqlservice.jaxb.Sqlmap;
 
 public class XmlSqlService implements SqlService {
 	private Map<String, String> sqlMap = new HashMap<>();
+	private String sqlmapFile;
 	
-	public XmlSqlService() {
+	public void setSqlmapFile(String sqlmapFile) {
+		this.sqlmapFile = sqlmapFile;
+	}
+	
+	@PostConstruct
+	public void loadSql() {
 		String contextPath = Sqlmap.class.getPackage().getName();
 		try {
 			JAXBContext context = JAXBContext.newInstance(contextPath);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+			InputStream is = UserDao.class.getResourceAsStream(sqlmapFile);
 			Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 			
 			for(SqlType sql : sqlmap.getSql()) {
