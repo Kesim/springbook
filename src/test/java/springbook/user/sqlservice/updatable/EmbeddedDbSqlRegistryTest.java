670adab5b7455fd70cb5e703f07475c6daf1ba99
Kang <tjdals9790@naver.com>
2021-10-25 15:17:38 +0900

    기능추가 - 트랜잭션 테스트 추가

--- src/test/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
+++ src/test/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -2,11 +2,18 @@ package springbook.user.sqlservice.updatable;
 
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import static org.junit.Assert.fail;
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.After;
+import org.junit.Test;
 
 import springbook.user.sqlservice.AbstractUpdatableSqlRegistryTest;
+import springbook.user.sqlservice.SqlUpdateFailureException;
 import springbook.user.sqlservice.UpdatableSqlRegistry;
 
 public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
@@ -28,5 +35,21 @@ public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest 
 	@After
 	public void tearDown() {
 		db.shutdown();
+	}
+	
+	@Test
+	public void transactionalUpdate() {
+		checkFindResult("SQL1", "SQL2", "SQL3");
+		
+		Map<String, String> sqlmap = new HashMap<>();
+		sqlmap.put("KEY1", "Modified1");
+		sqlmap.put("KEY9999!@#$", "Modified9999");
+		
+		try {
+			sqlRegistry.updateSql(sqlmap);
+			fail();
+		} catch(SqlUpdateFailureException e) {}
+		
+		checkFindResult("SQL1", "SQL2", "SQL3");
 	}
 }
