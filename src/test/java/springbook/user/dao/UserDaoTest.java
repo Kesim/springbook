9304666248972e3e2e100c0a58481a225298d153
Kang <tjdals9790@naver.com>
2021-10-26 11:20:53 +0900

    리팩토링 - 빈 설정을 xml 대신 자바 클래스로 변경하기 위해 일부 설정 변경

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -19,11 +19,12 @@ import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import springbook.user.TestApplicationContext;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "/test-applicationContext.xml")
+@ContextConfiguration(classes = TestApplicationContext.class)
 public class UserDaoTest {
 	@Autowired
 	private UserDao dao;
